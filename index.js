01a4d62cce647dc626dc52f618ed4a9f2ee70356
pleasant-calculator-7755 <domcode88@gmail.com>
2020-08-29 21:34:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+ var band = [];
+  for (let x = 0; x < 4; x +=1){
+    band.push( `${musicians[x]} plays ${instruments[x]}`);  
+  }
+  return band;
+}
+
+function johnLennonFacts(fact){
+      
+}
